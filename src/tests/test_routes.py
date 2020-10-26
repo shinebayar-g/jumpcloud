1804b5622608427e48f4747a93cb622cca25c5fa
shinebayar-g <shinebayar.gansukh@gmail.com>
2020-10-26 14:54:22 -0500

    test: update test

--- src/tests/test_routes.py
+++ src/tests/test_routes.py
@@ -3,20 +3,18 @@ import os
 import requests
 
 api_key = os.environ.get('X_API_KEY')
-request_base_url = 'http://127.0.0.1:5000/api/v1.0/'
+request_base_url = 'http://0.0.0.0:5000/api/v1.0/'
 request_headers = {'Accept': 'application/json', 'Content-Type': 'application/json',
                    'x-api-key': api_key}
-
-
-def test_get_systemusers():
-    response = requests.get(request_base_url + 'info',
-                            headers=request_headers)
-    assert response.status_code == 200
 
 
 def test_get_info():
     response = requests.get(request_base_url + 'info',
                             headers=request_headers)
-    print(response.text)
-    print(response.url)
+    assert response.status_code == 200
+
+
+def test_get_systemusers():
+    response = requests.get(request_base_url + 'systemusers',
+                            headers=request_headers)
     assert response.status_code == 200
